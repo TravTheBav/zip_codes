2ef476c0a91e1f1bb92be12583c927a1ba791153
zitot <zitot@oregonstate.edu>
2025-06-10 17:32:33 -0400

    Set up endpoints for creating, editing, and deleting locations

--- backend/server.mjs
+++ backend/server.mjs
@@ -14,7 +14,7 @@ const app = express()
 app.use(express.json())
 
 // Get all locations
-app.get("/", (req, res) => {
+app.get("/locations", (req, res) => {
     let query = 'SELECT * FROM Locations;'
 
     db.query(query, (error, rows) => {
@@ -24,6 +24,63 @@ app.get("/", (req, res) => {
         }
 
         res.json(rows)
+    })
+})
+
+// Create a new location
+app.post("/locations", (req, res) => {
+    let query = `INSERT INTO Locations (Zip, City, State, County, Latitude, Longitude)
+        VALUES (
+        ${req.body.zip},
+        ${req.body.city},
+        ${req.body.state},
+        ${req.body.county},
+        ${req.body.latitude},
+        ${req.body.longitude}
+        );`
+
+    // Create the INSERT query using values from the post body
+    db.query(query, (error) => {
+        if (error) {
+            console.log(error)
+            res.sendStatus(500)
+        }
+
+        res.status(201).send('A new location was successfully created.')
+    })
+})
+
+// Edit a location
+app.put("/locations", (req, res) => {
+    let query = `UPDATE Locations SET
+        Zip = ${req.body.zip},
+        City = '${req.body.city}',
+        State = '${req.body.state}',
+        County = '${req.body.county}',
+        Latitude = ${req.body.latitude},
+        Longitude = ${req.body.longitude};`
+
+    db.query(query, (error) => {
+        if (error) {
+            console.log(error)
+            res.sendStatus(500)
+        }
+
+        res.status(200).send('The location was successfully updated.')
+    })
+})
+
+// Delete a location
+app.delete("/locations/:_id", (req, res) => {
+    let query = `DELETE FROM Locations WHERE LocationID = ${req.params._id};`
+
+    db.query(query, (error) => {
+        if (error) {
+            console.log(error)
+            res.sendStatus(500)
+        }
+
+        res.status(200).send("The location was successfully deleted.")
     })
 })
 
